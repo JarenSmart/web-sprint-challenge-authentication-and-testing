311f502b8f4438d51ac79faacdfb8debd1c429e7
jaren <jarensmart@yahoo.com>
2020-07-19 19:12:48 -0400

    Added LOGIN & REGISTER & .ENV

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,60 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/api/register", async (req, res, next) => {
+  try {
+    const { username, password, department } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken",
+      });
+    }
+
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 14),
+      department,
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/api/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({
+        message: "You shall not pass!",
+      });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "You shall not pass!",
+      });
+    }
+
+    const tokenPayload = {
+      userId: user.id,
+      username: user.username,
+      userRole: "normal",
+    };
+
+    res.json({
+      message: `Welcome ${user.username}!`,
+      token: jwt.sign(tokenPayload, process.env.JWT_SECRET),
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
